afd80ffd1b4cbdc87b005dc3130aa58a19a017ee
gabby-m056 <144246046+gabby-m056@users.noreply.github.com>
2025-10-22 10:08:48 +0100

    slay

--- Assets/BallMovement.cs
+++ Assets/BallMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = -10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +13,6 @@ public class BallMovement : MonoBehaviour
     void Update()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(0.0f, speed,0.0f));
+        rb.AddForce(new Vector3(0.0f, 0.0f,speed));
     }
 }
